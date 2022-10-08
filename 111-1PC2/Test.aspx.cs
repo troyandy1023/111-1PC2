17d0d80093d1613a2ebddb11565dad3520cf5c4c
troyandy1023 <alicebobo223@gmail.com>
2022-10-08 17:46:46 +0800

    有考慮當歌手嗎我當然有考慮當歌手啊那是我的夢想耶我跟你說當歌手比當職業選手我當我當歌手是...最想當的

---
+++ 111-1PC2/Test.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC2
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+            {
+            int i_in = 66377;
+                double d_Ou = 0;
+                double d_i_In = i_in;
+                d_Ou = (9.0 / 5.0) * d_i_In + 32.0;
+                Response.Write(d_Ou);
+            }
+    }
+}
